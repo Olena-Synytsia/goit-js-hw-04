4f21592725a385bbb2d5bc437a6e3646a11dafd0
Olena-Synytsia <alenasinitsa85@gmail.com>
2024-06-07 20:57:22 +0300

    task 3

--- js/task-3.js
+++ js/task-3.js
@@ -1,29 +1,31 @@
 const profile = {
   username: "Jacob",
   playTime: 300,
-  amount: [],
+  amount: 0,
 
   changeUsername(newName) {
     this.username = newName;
   },
 
   updatePlayTime(hours) {
-    this.playTime += hours;
-    this.amount;
+    for (hour of hours) {
+      this.playTime += hours;
+      this.amount = this.playTime;
+    }
   },
 
   getInfo() {
-    return `${this.username} has ${amount} active hours!`;
+    return `${this.username} has ${this.amount} active hours!`;
   },
 };
 
-profile.playTime(amount);
 console.log(profile);
 
-// console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
 
 profile.changeUsername("Marco");
-// console.log(profile.getInfo()); // "Marco has 300 active hours!"
+console.log(profile.changeUsername);
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
 
 profile.updatePlayTime(20);
-// console.log(profile.getInfo()); // "Marco has 320 active hours!"
+console.log(profile.getInfo()); // "Marco has 320 active hours!
